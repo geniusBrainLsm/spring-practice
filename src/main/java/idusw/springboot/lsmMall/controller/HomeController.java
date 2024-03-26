d40a49a4882d408b0dd7d55375383d0805290519
unknown <geniusbrainlsm@gmail.com>
2024-03-26 12:55:14 +0900

    4th commit

--- src/main/java/idusw/springboot/lsmMall/controller/HomeController.java
+++ src/main/java/idusw/springboot/lsmMall/controller/HomeController.java
@@ -2,30 +2,34 @@ package idusw.springboot.lsmMall.controller;
 
 import idusw.springboot.lsmMall.model.Member;
 import idusw.springboot.lsmMall.service.HomeService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
 
-    private HomeService homeService;
-    public HomeController(HomeService homeService){   // loosely-coupled <- Ioc Container, DI
-         this.homeService = homeService;
-    }
+    private final HomeService homeService;
 
-    @GetMapping(value = {"/", "/1"}) // Get Method 요청으로 처리하라는 annotation
-    public String getRoot(){
+    @GetMapping(value = {"/", "/1"})
+    public String getRoot(HttpServletRequest request, Model model){
         homeService.sayHello();
-        // model : dto (data transfer object) - 데이터를 주고받기 위한 목적
-        // -- model --> service -- model --> repository -> service -> controller -> view
+        model.addAttribute("greeting", request.getParameter("name"));
         return "main/index"; // view
     }
+
+
     @GetMapping("/2")
-    public String getIndex2(Model model){
+    public String getIndex2(@RequestParam Map<String, String> param , Model model){
         Member member = new Member();
         member.setId("lsm");
-        member.setName("이상민");
+        member.setName(param.get("name"));
         member.setEmail("asd@asd.asd");
 
         // spring에서 Ui를 통해  view에게 전달할 때 사용하는 객체: Model model
--- src/main/java/idusw/springboot/lsmMall/controller/MemberController.java
+++ src/main/java/idusw/springboot/lsmMall/controller/MemberController.java
@@ -1,5 +1,44 @@
 package idusw.springboot.lsmMall.controller;
 
+import idusw.springboot.lsmMall.model.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
 public class MemberController {
+    @GetMapping("members/{id}")
+    public String getById(@PathVariable String id, Model model){
+        Member member = new Member();
+        member.setId(id);
+        model.addAttribute("member", member);
+        return "./main/index";
+    }
+
+    @GetMapping("members/login")
+    public String getLogin(){
+
+        return "./main/login";
+    }
+
+    @PostMapping("members/login")
+    public String postLogin(@ModelAttribute("member") Member member, Model model){
+        String id = member.getId();
+        String pw = member.getPw();
+        Member m = new Member();
+        if(id.equals("induk") && pw.equals("comso")){
+            m.setId("success");
+            model.addAttribute("member", m);
+        }
+        else
+        {
+            m.setId("fail");
+            model.addAttribute("member", m);
+        }
+
+        return "./main/index";
+    }
 
 }
